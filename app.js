57a230495ab5ffa22eb605a70f55a0fac57a726d
DariusAndrijauskas <Darius.Andrijauskas@gmail.com>
2021-10-24 11:36:05 +0300

    some minor cleanup

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-//const {delay} = require("lodash");
 const app = express();
 
 app.set('view engine', 'ejs');
